dd01f89dd4b8ac0f89a870cd1eaa8c5ce473adf4
maxmillionsmax <maximilianocaetano@gmail.com>
2022-08-07 01:11:01 -0300

    microserviço de cadastro insert,update,delete

--- src/main/java/com/br/myfood/cadastro/controller/ClienteController.java
+++ src/main/java/com/br/myfood/cadastro/controller/ClienteController.java
@@ -1,21 +1,52 @@
 package com.br.myfood.cadastro.controller;
 
 import com.br.myfood.cadastro.dto.ClienteDto;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.br.myfood.cadastro.entity.Client;
+import com.br.myfood.cadastro.service.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Objects;
 
 @RequestMapping("/client")
 @RestController
 public class ClienteController {
 
-    @PostMapping("/insert")
-    public String insertClient(ClienteDto clienteDto){
 
-
-
-        return "OK";
+    private final ClienteService clienteService;
+    @Autowired
+    public ClienteController(ClienteService clienteService) {
+        this.clienteService = clienteService;
     }
 
+    @PostMapping("/insert")
+    public ResponseEntity insertClient(@RequestBody ClienteDto clienteDto) {
+
+        try {
+            return ResponseEntity.ok(clienteService.inserClient(Client.create(clienteDto)));
+        } catch (Exception e) {
+           return ResponseEntity.badRequest().body(e);
+        }
+    }
+    @PutMapping("/update/{id}")
+    public ResponseEntity updateClient(@PathVariable("id") Long id, @RequestBody ClienteDto clienteDto){
+
+        Client client = Client.create(clienteDto);
+        client.setId(id);
+
+        Client updateClient = clienteService.updateClient(client);
+
+        return Objects.nonNull(updateClient) ?
+               ResponseEntity.ok(updateClient) :
+               ResponseEntity.notFound().build();
+
+    }
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity deleteClient(@PathVariable("id") Long id){
+        return clienteService.deleteClient(id) ?
+                ResponseEntity.ok().build() :
+                ResponseEntity.notFound().build();
+    }
 }
 
